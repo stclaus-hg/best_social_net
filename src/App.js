f158d6530f02e7d84dd33bced7f9846faabd4932
Alexander Voitenko <alexander@blackswan-technologies.com>
2024-02-05 21:47:34 +0200

    add dialogs page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import Dialogs from './Components/Dialogs/Dialogs';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
 
 function App() {
-  return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <Header />
+            <Navbar />
+            {/* <Profile /> */}
+            <div className='app-wrapper-content'>
+                <Dialogs />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Dialogs/Dialogs.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Dialogs extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                Dialogs
+            </div>
+        );
+    }
+}
+ 
+export default Dialogs;
