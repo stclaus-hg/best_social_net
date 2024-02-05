fe0e48588489f6c339acc23f65d8d4e2098cded1
Alexander Voitenko <alexander@blackswan-technologies.com>
2024-02-05 12:23:22 +0200

    add simple layout

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       </nav>
       <div className='content'>
         <div>
-            <img src='' />
+            <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
         </div>
         <div>
             ava + description
